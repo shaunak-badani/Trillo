265cb2c68c8ce006a8e118e5319bd10138217010
Shaunak Badani <shaunak.badani@duke.edu>
2026-06-26 03:13:25 -0400

    Add images and css  variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.scss";
 
 
 
@@ -20,7 +20,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet" />
         <link rel="stylesheet" href="css/style.css" />
-        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <link rel="shortcut icon" type="image/png" href="/img/favicon.png" />
         
         <title>trillo &mdash; Your all-in-one booking app</title>
       </head>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <div className={styles.page}>
-      Hi
+      Trillo
     </div>
   );
 }
