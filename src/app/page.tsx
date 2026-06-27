25767e9a97e2214d0347479afc3af92c150f922f
Shaunak Badani <shaunak.badani@duke.edu>
2026-06-27 07:56:02 -0400

    Completed user reviews section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import "./page.scss";
 import "./components.scss";
+import "./layout.scss";
 import Link from "next/link";
 
 export default function Home() {
@@ -118,6 +119,71 @@ export default function Home() {
             <div className="overviewRating">
               <div className="overviewRatingAverage">8.6</div>
               <div className="overviewRatingCount">429 votes</div>
+            </div>
+          </div>
+          <div className="detail">
+            <div className="description">
+              <p className="paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi nisi dignissimos debitis ratione sapiente saepe. Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate.
+              </p>
+              <p className="paragraph">
+                Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate delectus, inventore iure aliquid aliquam.
+              </p>
+              <ul className="list">
+                <li className="listItem">Close to the beach</li>
+                <li className="listItem">Breakfast included</li>
+                <li className="listItem">Free airport shuttle</li>
+                <li className="listItem">Free wifi in all rooms</li>
+                <li className="listItem">Air conditioning and heating</li>
+                <li className="listItem">Pets allowed</li>
+                <li className="listItem">We speak all languages</li>
+                <li className="listItem">Perfect for faimilies</li>
+
+              </ul>
+              <div className="recommend">
+                <p className="recommendCount">
+                  Lucy and 3 other friends recommend this hotel.
+                </p>
+                <div className="recommendFriends">
+                  <img src="img/user-3.jpg" alt="Friend 1" className="recommendPhoto" />
+                  <img src="img/user-4.jpg" alt="Friend 2" className="recommendPhoto" />
+                  <img src="img/user-5.jpg" alt="Friend 3" className="recommendPhoto" />
+                  <img src="img/user-6.jpg" alt="Friend 4" className="recommendPhoto" />
+                </div>
+              </div>
+            </div>
+            <div className="userReviews">
+              <figure className="review">
+                <blockquote className="reviewText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi, totam, itaque officia ex.
+                </blockquote>
+                <figcaption className="reviewUser">
+                  <img src="img/user-1.jpg" alt="User 1" className="reviewPhoto" />
+                  <div className="reviewUserBox">
+                    <p className="reviewUserName">Nick Smith</p>
+                    <p className="reviewUserDate">Feb 23rd, 2017</p>
+                  </div>
+                  <div className="reviewRating">7.8</div>
+                </figcaption>
+              </figure>
+
+              <figure className="review">
+                <blockquote className="reviewText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi.
+                </blockquote>
+                <figcaption className="reviewUser">
+                  <img src="img/user-2.jpg" alt="User 2" className="reviewPhoto" />
+                  <div className="reviewUserBox">
+                    <p className="reviewUserName">Mary Thomas</p>
+                    <p className="reviewUserDate">Sept 13, 2017</p>
+                  </div>
+                  <div className="reviewRating">9.3</div>
+                </figcaption>
+              </figure>
+
+              <button className="btnInline">
+                Show all <span>&rarr;</span>
+              </button>
             </div>
           </div>
         </main>
